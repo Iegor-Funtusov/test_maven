cf20f5e5ba0382036491be9203a6354c48085246
funtushan@gmail.com <funtushan@gmail.com>
2020-06-19 21:08:20 +0300

    test 1

--- calc-util/src/main/java/ua/com/alevel/nix/maventest/calc/CalculationUtil.java
+++ calc-util/src/main/java/ua/com/alevel/nix/maventest/calc/CalculationUtil.java
@@ -3,6 +3,7 @@ package ua.com.alevel.nix.maventest.calc;
 public class CalculationUtil {
 
     public static int sum(int left, int right) {
+        System.out.println("CalculationUtil.sum");
         return left + right;
     }
 }
