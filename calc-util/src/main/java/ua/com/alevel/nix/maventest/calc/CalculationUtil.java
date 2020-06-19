4fa6eb5273a61c5b73252df1be904fc73fc212b3
funtushan@gmail.com <funtushan@gmail.com>
2020-06-19 20:23:27 +0300

    first commit

---
+++ calc-util/src/main/java/ua/com/alevel/nix/maventest/calc/CalculationUtil.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.nix.maventest.calc;
+
+public class CalculationUtil {
+
+    public static int sum(int left, int right) {
+        return left + right;
+    }
+}
---
+++ calc-view/src/main/java/ua/com/alevel/nix/maventes/view/Main.java
@@ -0,0 +1,11 @@
+package ua.com.alevel.nix.maventes.view;
+
+import ua.com.alevel.nix.maventest.calc.CalculationUtil;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+        System.out.println("sum = " + CalculationUtil.sum(6, 9));
+    }
+}
